253b5fca8ae1496fdccac5d44ef8a3d3c073794b
oofaungoo <faung97@gmail.com>
2025-02-24 23:03:31 +0700

    update UI: ปรับปรุงหน้าจอค้นหาออเดอร์

---
+++ .history/FoodMood/src/pages/MenuManager/assets/AddIngredient_20250213050930.jsx
@@ -0,0 +1,163 @@
+import React, { useState,useEffect} from 'react';
+
+const AddIngredient = ({
+    currentSize,
+    newSizePrice,
+    ingredientOptions,
+    onUpdateSizeName,
+    onUpdatePrice,
+    onBack,
+    handleAddIngredient,
+    ingredientMain
+}) => {
+    const [ingredients, setIngredients] = useState([
+        { 
+            newIngredient: '', 
+            newIngredientQty: '', 
+            unit: 'kg' 
+        },
+    ]);
+    useEffect(() => {
+        if(ingredientMain){
+            setIngredients(ingredientMain);
+        }
+    }, []);
+
+    const [editedSizeName, setEditedSizeName] = useState(currentSize);
+    const [editedPrice, setEditedPrice] = useState(newSizePrice);
+
+    const [propIngredientOptions, setPropIngredientOptions] = useState(ingredientOptions);
+
+    const handleAddMoreFields = () => {
+        setIngredients([
+            ...ingredients,
+            { newIngredient: '', newIngredientQty: '', unit: 'kg' },
+        ]);
+    };
+
+    const handleUpdateSizeName = () => {
+        if (editedSizeName && editedSizeName !== currentSize) {
+            onUpdateSizeName(currentSize, editedSizeName);
+        }
+    };
+
+    const handleUpdatePrice = () => {
+        console.log("Updating price:", editedSizeName, editedPrice);
+        if (editedPrice && typeof onUpdatePrice === "function") {
+            onUpdatePrice(editedSizeName, editedPrice);
+        } else {
+            console.error("onUpdatePrice is not a function or missing");
+        }
+    };
+    
+
+    const updateIngredientField = (index, field, value) => {
+        const updatedIngredients = [...ingredients];
+        if(field == "newIngredient"){
+            const targetIngredient = propIngredientOptions.find(option => option.name === value);
+            console.log('targetIngredient:', targetIngredient);
+            updatedIngredients[index][field] = value;
+            updatedIngredients[index]["unit"] = targetIngredient.unit;
+
+        }else{
+            updatedIngredients[index][field] = value;
+        }
+        console.log('index:', index);
+        console.log('value:', value);
+        console.log('Updated Ingredients:', updatedIngredients);
+
+        setIngredients(updatedIngredients);
+    };
+
+    const handleSave = () => {
+        const dataToSave = {
+            sizeName: editedSizeName,
+            price: editedPrice,
+            ingredients: ingredients,
+        };
+        handleAddIngredient(dataToSave);
+        onBack();
+    };
+
+    return (
+        <div>
+            <div className="right-box-header text-center fs-18 fw-5">
+                เพิ่มวัตถุดิบ
+            </div>
+            <div style={{ marginTop: "10px" }}>
+                <label>ขนาด </label>
+                <input
+                    type="text"
+                    value={editedSizeName}
+                    onChange={(e) => setEditedSizeName(e.target.value)}
+                    onBlur={handleUpdateSizeName} // Update size name on blur
+                />
+            </div>
+            <div style={{ marginTop: "10px" }}>
+                <label>ราคา </label>
+                <input
+                    type="number"
+                    value={editedPrice}
+                    placeholder="ระบุราคา"
+                    onChange={(e) => setEditedPrice(e.target.value)}
+                    onBlur={handleUpdatePrice} // Update price on blur
+                />
+            </div>
+            <div style={{ marginTop: "10px" }}>
+                <label>วัตถุดิบ</label>
+                {ingredients.map((ingredient, index) => (
+                    <div key={index} className="inline-elements" style={{ marginBottom: "10px" }}>
+                        <select
+                            className="form-select"
+                            value={ingredient.newIngredient}
+                            onChange={(e) =>
+                                updateIngredientField(index, 'newIngredient', e.target.value)
+                            }
+                        >
+                            <option disabled value="">เลือกวัตถุดิบ</option>
+                            {ingredientOptions.map((option, idx) => (
+                                <option key={idx} value={option.name}>
+                                    {option.name}
+                                </option>
+                            ))}
+                        </select>
+                        <input
+                            type="number"
+                            placeholder="ปริมาณ"
+                            value={ingredient.newIngredientQty}
+                            onChange={(e) =>
+                                updateIngredientField(index, 'newIngredientQty', e.target.value)
+                            }
+                        />
+                        <select
+                            value={ingredient.unit}
+                            disabled
+                            onChange={(e) =>
+                                updateIngredientField(index, 'unit', e.target.value)
+                            }
+                            
+                        >
+                            <option value="kg">กิโลกรัม</option>
+                            <option value="g">กรัม</option>
+                        </select>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <button className="circle-plus-btn" onClick={handleAddMoreFields}>+</button>
+            </div>
+
+            <div className="order-action-buttons">
+                <button type="button" className="red-button" onClick={onBack}>
+                    ย้อนกลับ
+                </button>
+                <button type="button" className="blue-button" onClick={handleSave}>
+                    บันทึก
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default AddIngredient;
